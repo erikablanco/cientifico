5cd2700352f0c3636a7dbaa5fc2ff6cb6cb67cf3
erikablanco <erika.blanco@alteru.co>
2021-07-02 20:06:46 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require ('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { resolve } = require('path');
-const  CopyWebpackPlugin = require('copy-webpack-plugin'); 
+const CopyWebpackPlugin = require('copy-webpack-plugin'); 
 
 
 module.exports = {
